a17f9158b0199ed32293173fc168279d7f78545b
Jiwon Jung <devziwon@gmail.com>
2022-12-05 09:14:01 +0900

    set up in Swift

---
+++ Muscat/Renderer.swift
@@ -0,0 +1,66 @@
+//
+//  Renderer.swift
+//  Muscat
+//
+//  Created by Jiwon Jung on 2022/12/05.
+//
+
+import Foundation
+import MetalKit
+
+class Renderer: NSObject {
+    static var device: MTLDevice!
+    let commandQueue: MTLCommandQueue
+    static var library: MTLLibrary!
+    let pipelineState: MTLRenderPipelineState
+    
+    init(view: MTKView) {
+        guard let device = MTLCreateSystemDefaultDevice(),
+              let commandQueue = device.makeCommandQueue() else {
+            fatalError("Unable to connect to GPU")
+        }
+        Renderer.device = device
+        self.commandQueue = commandQueue
+        Renderer.library = device.makeDefaultLibrary()!
+        pipelineState = Renderer.createPipelineState()
+        super.init()
+    }
+    
+    static func createPipelineState() -> MTLRenderPipelineState {
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        
+        // pipeline state properties
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        let vertexFunction = Renderer.library.makeFunction(name: "vertex_main")
+        let fragmentFunction = Renderer.library.makeFunction(name: "fragment_main")
+        pipelineStateDescriptor.vertexFunction = vertexFunction
+        pipelineStateDescriptor.fragmentFunction = fragmentFunction
+        
+        
+        return try! Renderer.device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+    }
+}
+
+extension Renderer: MTKViewDelegate {
+    // resize, rotate io device
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    // gpu interaction
+    func draw(in view: MTKView) {
+        guard let commandBuffer = commandQueue.makeCommandBuffer(),
+              let drawable = view.currentDrawable,
+              let descriptor = view.currentRenderPassDescriptor,
+              let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor) else {
+            return
+        }
+        
+        commandEncoder.setRenderPipelineState(pipelineState)
+        commandEncoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: 1)
+        commandEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+}
---
+++ Muscat/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Muscat
+//
+//  Created by Jiwon Jung on 2022/12/05.
+//
+
+import Cocoa
+import MetalKit
+class ViewController: NSViewController {
+    @IBOutlet var metalView: MTKView!
+    var renderer: Renderer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        renderer = Renderer(view: metalView)
+        metalView.device = Renderer.device
+        metalView.delegate = renderer
+        
+        metalView.clearColor = MTLClearColor(red: 1.0, green: 1.0, blue: 0.8, alpha: 1.0)
+        
+    }
+
+}
+
