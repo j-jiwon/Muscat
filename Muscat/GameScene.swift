f352f284d0d4f2510db42a7fa37363b5eb6cd0d1
Jiwon Jung <devziwon@gmail.com>
2023-01-16 21:29:53 +0900

    challenge: Render all the trains with random positions and rotations

--- Muscat/GameScene.swift
+++ Muscat/GameScene.swift
@@ -8,18 +8,22 @@
 import Foundation
 
 class GameScene: Scene {
-    let train = Model(name: "train")
+    let trains = Instance(name: "train", instanceCount: 100)
     let trees = Instance(name: "treefir", instanceCount: 100)
 
     override func setupScene() {
         camera.target = [0, 0.8, 0]
-        camera.distance = 4
+        camera.distance = 8
         camera.rotation = [-0.4, -0.4, 0]
     
         add(node: trees)
-        add(node: train)
+        add(node: trains)
         
         for i in 0..<100 {
+            trains.transforms[i].position.x = Float.random(in: -5..<5)
+            trains.transforms[i].position.z = Float.random(in: 0..<10)
+            trains.transforms[i].rotation.y = Float.random(in: 0..<radians(fromDegrees: 359))
+            
             trees.transforms[i].position.x = Float(i) - 50
             trees.transforms[i].position.z = 2
         }
