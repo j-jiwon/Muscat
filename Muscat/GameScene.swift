2747b5e99ef62a06ba1c4385db8fe9c8466d5ed0
Jiwon Jung <devziwon@gmail.com>
2023-01-17 19:45:35 +0900

    add assets for raybreak game

--- Muscat/GameScene.swift
+++ Muscat/GameScene.swift
@@ -8,24 +8,10 @@
 import Foundation
 
 class GameScene: Scene {
-    let trains = Instance(name: "train", instanceCount: 100)
-    let trees = Instance(name: "treefir", instanceCount: 100)
 
     override func setupScene() {
         camera.target = [0, 0.8, 0]
         camera.distance = 8
         camera.rotation = [-0.4, -0.4, 0]
-    
-        add(node: trees)
-        add(node: trains)
-        
-        for i in 0..<100 {
-            trains.transforms[i].position.x = Float.random(in: -5..<5)
-            trains.transforms[i].position.z = Float.random(in: 0..<10)
-            trains.transforms[i].rotation.y = Float.random(in: 0..<radians(fromDegrees: 359))
-            
-            trees.transforms[i].position.x = Float(i) - 50
-            trees.transforms[i].position.z = 2
-        }
     }
 }
